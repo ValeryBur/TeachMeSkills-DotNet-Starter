7807422702260405bfd0111cd370008a2d7dc6cc
ValeryBur <valerik_utd@mail.ru>
2020-09-18 21:31:48 +0300

    feat: add solution of task3

---
+++ src/TeachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal pi = 3.141592653M;
+            decimal r = 2.0M;
+            decimal s = pi * r * r;
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
