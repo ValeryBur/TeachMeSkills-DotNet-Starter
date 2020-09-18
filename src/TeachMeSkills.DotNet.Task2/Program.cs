f81b2bdf38f17d25bf7b608f8a2cfd04d8c163c1
ValeryBur <valerik_utd@mail.ru>
2020-09-18 21:19:55 +0300

    feat: add solution of 2 task

---
+++ src/TeachMeSkills.DotNet.Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "\nмоя строка 1";//переход на новую строку
+            string str2 = "\tмоя строка 2";//табуляция - небольшой доступ
+            string str3 = "\aмоя строка 3";//звуковой сигнал
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.ReadLine();
+        }
+    }
+}
