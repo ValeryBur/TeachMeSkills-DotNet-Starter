194042fde1dcb1612924b0f9740f0400dd4aaa4e
ValeryBur <valerik_utd@mail.ru>
2020-09-18 21:02:25 +0300

    feat: add solution of 1 task

--- src/TeachMeSkills.DotNet.Task1/Program.cs
+++ src/TeachMeSkills.DotNet.Task1/Program.cs
@@ -7,7 +7,11 @@ namespace TeachMeSkills.DotNet.Task1
         static void Main(string[] args)
         {
             // Your code here..
+            decimal pi = 3.141592653M;
+            decimal e = 2.7182818284590452M;
 
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
             Console.ReadLine();
         }
     }
