5e75253968d3567f9e7a598c43d27d6c2f0da904
ValeryBur <valerik_utd@mail.ru>
2020-09-19 11:27:43 +0300

    feat: add solution of task8

---
+++ src/TeachMeSkills.DotNet.Task8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Translator!");
+            Console.Write("Input word: ");
+            string userInput = Console.ReadLine();
+            if (userInput == "солнце")
+            {
+                Console.WriteLine("Translate: sun");
+            }
+            else if (userInput == "дождь")
+            {
+                Console.WriteLine("Translate: rain");
+            }
+            else if (userInput == "ветер")
+            {
+                Console.WriteLine("Translate: wind");
+            }
+            else if (userInput == "свежий")
+            {
+                Console.WriteLine("Translate: fresh");
+            }
+            else if (userInput == "жара")
+            {
+                Console.WriteLine("Translate: heat");
+            }
+            else if (userInput == "теплый")
+            {
+                Console.WriteLine("Translate: warm");
+            }
+            else if (userInput == "облачный")
+            {
+                Console.WriteLine("Translate: cloudy");
+            }
+            else if (userInput == "морозный")
+            {
+                Console.WriteLine("Translate: frosty");
+            }
+            else if (userInput == "бриз")
+            {
+                Console.WriteLine("Translate: breeze");
+            }
+            else if (userInput == "шторм")
+            {
+                Console.WriteLine("Translate: storm");
+            }
+            else
+            {
+                Console.WriteLine("Unknown word!");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
