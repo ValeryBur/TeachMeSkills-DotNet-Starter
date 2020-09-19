5f797e495841698accb6ade309297c5e8f6d2335
ValeryBur <valerik_utd@mail.ru>
2020-09-20 01:52:43 +0300

    feat: add solution of task11

---
+++ src/TeachMeSkills.DotNet.Task11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint checkValue = InputNumber();
+            bool result;
+            for (int i = 1; i < 33; i++)
+            {
+                if(checkValue==Math.Pow(2, i))
+                {
+                    result = true;
+                    Console.WriteLine($"Result is: {result}");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.ReadLine();
+        }
+        public static uint InputNumber()
+        {
+            Console.Write("Input your number: ");
+            string userInput = Console.ReadLine();
+            bool canParse = false;
+            canParse = uint.TryParse(userInput, out uint parsedNumber);
+            return parsedNumber;
+        }
+    }
+}
