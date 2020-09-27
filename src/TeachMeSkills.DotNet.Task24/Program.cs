aed87c3962d927a25e88060045512d90bb1b5b76
ValeryBur <valerik_utd@mail.ru>
2020-09-27 16:04:02 +0300

    feat: add solution of Task24

---
+++ src/TeachMeSkills.DotNet.Task24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace TeachMeSkills.DotNet.Task24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input quantity of array elements: ");
+            Random random = new Random();
+            int arrayLength = ElementsInput();
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 5);
+                if (array[i] % 2 != 0)
+                {
+                    Console.WriteLine($"Odd number is: {array[i]}");
+                }
+            }
+            Console.WriteLine($"Max value is: {array.Max()}");
+            Console.WriteLine($"Min value is: {array.Min()}");
+            Console.WriteLine($"Array sum is: {array.Sum()}");
+            Console.WriteLine($"Average value is: {array.Average()}");
+
+
+
+            Console.ReadLine();
+        }
+        public static int ElementsInput()
+        {
+            string str = Console.ReadLine();
+            bool canParse = false;
+            canParse = int.TryParse(str, out int parsedValue);
+            return parsedValue;
+        }
+    }
+}
