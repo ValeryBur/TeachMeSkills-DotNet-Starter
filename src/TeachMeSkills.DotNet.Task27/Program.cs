71ca31ef06e2d853b15e7ba509c8dbe734cf3789
ValeryBur <valerik_utd@mail.ru>
2020-09-27 21:22:34 +0300

    feat: add solution of task27

---
+++ src/TeachMeSkills.DotNet.Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TeachMeSkills.DotNet.Task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 30);
+            }
+            Array.Reverse(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"\t{item}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
