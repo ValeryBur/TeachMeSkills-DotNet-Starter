3c85264841148fec3453162c89aec8df3bd63103
ValeryBur <valerik_utd@mail.ru>
2020-09-28 21:34:01 +0300

    feat: add solution of task26

--- src/TeachMeSkills.DotNet.Task26. Not Ready/Program.cs
+++ src/TeachMeSkills.DotNet.Task26. Not Ready/Program.cs
@@ -6,7 +6,47 @@ namespace TeachMeSkills.DotNet.Task26._Not_Ready
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            int newSize1 = 6;
+            Array.Resize(ref arr, newSize1);
+            arr[5] = 6;
+            foreach (var item in arr)
+            {
+                Console.Write($"\t{item}");
+            }
+            Console.ReadLine();
+
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int value = 10;
+            int [] remakeArray = RemakeArray(arr1, value);
+            foreach (var item in remakeArray)
+            {
+                Console.Write($"\t{item}");
+            }
+            Console.ReadLine();
+        }
+        public static int[] RemakeArray(int[] arr1, int value)
+        {
+            int newSize2 = 6;
+            Array.Resize(ref arr1, newSize2);
+            int[] newArr = arr1;
+            int tempValue1 = 0;
+            int tempValue2 = 0;
+            for (int i = 0; i < newSize2; i++)
+            {
+                if (i < 1)
+                {
+                    tempValue1 = newArr[i];
+                    newArr[i] = value;
+                }
+                else
+                {
+                    tempValue2 = newArr[i];
+                    newArr[i] = tempValue1;
+                    tempValue1 = tempValue2;
+                }
+            }
+            return newArr;
         }
     }
 }
