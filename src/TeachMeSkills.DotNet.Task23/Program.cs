a12fdee9549827cd11698efa1956f89a11dd4abb
ValeryBur <valerik_utd@mail.ru>
2020-10-13 12:27:52 +0300

    feat: add solution of task23

--- src/TeachMeSkills.DotNet.Task23/Program.cs
+++ src/TeachMeSkills.DotNet.Task23/Program.cs
@@ -4,9 +4,26 @@ namespace TeachMeSkills.DotNet.Task23
 {
     class Program
     {
+
+        //habr.com/ru/post/275813/ - изучить похожие задачи!
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите количество клиентов: ");
+            long clientsCount = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(RecursionFunction(clientsCount));
+            Console.ReadLine();
+        }
+        public static long RecursionFunction(long clientsCount)
+        {
+            // условие выхода
+            // Базовый случай
+            // когда остановиться повторять рекурсию ?
+            if (clientsCount == 1)
+            {
+                return 1;
+            }
+            // Шаг рекурсии / рекурсивное условие
+            return RecursionFunction(clientsCount - 1) * clientsCount;
         }
     }
 }
