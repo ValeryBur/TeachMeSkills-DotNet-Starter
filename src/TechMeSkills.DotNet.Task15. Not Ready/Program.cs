4c45ebfa0598a70b411134f98a66e7a5c3048b9a
ValeryBur <valerik_utd@mail.ru>
2020-10-13 11:54:48 +0300

    feat: add solution of task 15

--- src/TechMeSkills.DotNet.Task15. Not Ready/Program.cs
+++ src/TechMeSkills.DotNet.Task15. Not Ready/Program.cs
@@ -6,7 +6,17 @@ namespace TechMeSkills.DotNet.Task15._Not_Ready
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите число клиентов: ");
+            long clientCount = Convert.ToInt64(Console.ReadLine());
+            long result = 1;
+
+            do
+            {
+                result *= clientCount;
+                clientCount -= 1;
+            } while (clientCount > 0);
+            Console.WriteLine($"Количество вариантов доставки: {result}");
+            Console.ReadKey();
         }
     }
 }
