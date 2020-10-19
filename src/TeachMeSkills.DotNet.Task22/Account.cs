8e933e50cee5eb21784b1c39ea018af26bbec044
ValeryBur <valerik_utd@mail.ru>
2020-10-19 16:06:46 +0300

    feat: add solution of Task22

---
+++ src/TeachMeSkills.DotNet.Task22/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TeachMeSkills.DotNet.Task22
+{
+    class Account
+    {
+        public event Action<string> Notify;
+        private double _loanSum;
+        private double _overPay;
+        public double LoanSum { get { return _loanSum; } set { _loanSum = value; } }
+
+
+        public void PayLoan(double payment)
+        {
+            if (payment < _loanSum)
+            {
+                _loanSum -= payment;
+                Notify?.Invoke($"Loan sum is: {_loanSum}\nOverpay is: 0");
+            }
+            else if (payment == _loanSum)
+            {
+                _loanSum -= payment;
+                Notify?.Invoke($"Loan sum is: {_loanSum}\nOverpay is: 0");
+            }
+
+            else if (payment > _loanSum)
+            {
+                _overPay = payment - _loanSum;
+                Notify?.Invoke($"Loan sum is missing\nOverpay is: {_overPay}");
+            }
+            else
+            {
+                Notify?.Invoke($"Loan sum is: {_loanSum}\nOverpay is: 0");
+            }
+        }
+
+        public bool InputContinue()
+        {
+            Console.Write("Do you want to add another payment? Press Y/y");
+            var key = Console.ReadKey().Key;
+            Console.WriteLine();
+            return key == ConsoleKey.Y;
+        }
+
+    }
+}
+
+
--- src/TeachMeSkills.DotNet.Task22/Program.cs
+++ src/TeachMeSkills.DotNet.Task22/Program.cs
@@ -6,89 +6,31 @@ namespace TeachMeSkills.DotNet.Task22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Credit Information");
-            DateTime dateOfLoan = new DateTime(2020, 1, 1);
-            Console.WriteLine($"Date of loan is: {dateOfLoan}");
-            DateTime loanEndDate = dateOfLoan.AddMonths(7);
-            Console.WriteLine($"End date of loan is: {loanEndDate}");
-            double loanSum = 700;
-            Console.WriteLine($"Loan sum is: {loanSum}");
-            double monthlyPayment = 100;
-            Console.WriteLine($"Monthly payment is: {monthlyPayment}");
-            Console.Write("Input payment value: ");
-            double paymentValue = InputPaymentValue();
-            Console.Write("Input payment date: ");
-            DateTime paymentDate = InputPaymentDate();
-            (double overPay, double debt) = LoanBalance(dateOfLoan, loanEndDate, loanSum, monthlyPayment, paymentValue, paymentDate);
+            Account SomeAccount = new Account();
+            SomeAccount.Notify += BalanceInvoker;
+            Console.Write("Input loan sum: ");
+            SomeAccount.LoanSum = UserInput();
+            do
+            {
+                Console.Write("Input the payment: ");
+                SomeAccount.PayLoan(UserInput());
+            }
+            while (SomeAccount.InputContinue());
             Console.ReadLine();
-
         }
-        public static (double overPay, double debt) LoanBalance(DateTime dateOfLoan, DateTime loanEndDate,
-                       double loanSum, double monthlyPayment, double paymentValue, DateTime paymentDate)
-        {
-
-            double overPay;
-            double debt;
-            double loanBalance = loanSum - paymentValue;
-            //int paysCount = 0;
-
-
-
-            if (paymentValue == monthlyPayment && paymentDate <= dateOfLoan.AddMonths(1))
-            {
-                overPay = 0;
-                debt = 0;
-                //paysCount++;
-                Console.WriteLine($"Overpay is: {overPay}");
-                Console.WriteLine($"Debt is: {debt}");
-                Console.WriteLine("The debt is missing!");
-                return (overPay, debt);
-            }
-            else if (paymentValue < monthlyPayment && paymentDate < dateOfLoan.AddMonths(1))
-            {
-                overPay = 0;
-                debt = monthlyPayment - paymentValue;
-                Console.WriteLine($"Overpay is: {overPay}");
-                Console.WriteLine($"Debt is: {debt}");
-                return (overPay, debt);
-            }
-            else if (paymentValue > monthlyPayment && paymentDate <= dateOfLoan.AddMonths(1))
-            {
-                overPay = paymentValue - monthlyPayment;
-                debt = 0;
-                Console.WriteLine($"Overpay is: {overPay}");
-                Console.WriteLine($"Debt is: {debt}");
-                Console.WriteLine("The debt is missing!");
-                return (overPay, debt);
-            }
-            else
-            {
-                if (paymentDate > dateOfLoan.AddMonths(3))
-                {
-
-                }
-                debt = monthlyPayment;
-                overPay = 0;
-                Console.WriteLine($"The debt is: {debt}!");
-                return (overPay, debt);
-            }
-
-        }
-        public static double InputPaymentValue()
+        public static double UserInput()
         {
             string str = Console.ReadLine();
             bool canParse = false;
             canParse = double.TryParse(str, out double parsedValue);
             return parsedValue;
         }
-        public static DateTime InputPaymentDate()
+        public static void BalanceInvoker(string mes)
         {
-            string str = Console.ReadLine();
-            bool canParse = false;
-            canParse = DateTime.TryParse(str, out DateTime parsedValue);
-            return parsedValue;
+            Console.WriteLine(mes);
         }
 
     }
 }
+
 
